7c8732c8579d91c98ac072f1218d30eb133a7c64
chrissayon <Christopher.L.Sayon@gmail.com>
2019-09-23 01:45:46 +1000

    Notification window for alert done

---
+++ src/reducers/notificationReducer.js
@@ -0,0 +1,33 @@
+import { DESC_API, PORT_STATS_API, FLOW_STATS_API, FLOW_AGG_STATS_API } from '../actions/types';
+
+const initialState = {
+    
+};
+
+
+export default function(state = initialState, action) {
+    switch(action.type) {
+        case DESC_API:
+            return {
+                ...state,
+                descData : action.payload
+            }
+        case FLOW_AGG_STATS_API:
+            return {
+                ...state,
+                flowAggData: action.payload
+            }
+        case PORT_STATS_API:
+            return {
+                ...state,
+                portData: action.payload
+            }
+        case FLOW_STATS_API:
+            return {
+                ...state,
+                flowData: action.payload
+            }
+        default:
+            return state;
+    }
+}
