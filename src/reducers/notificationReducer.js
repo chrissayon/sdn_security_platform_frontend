75fc32e618cc77f59afe49ca48ab960e1708ee4f
chrissayon <Christopher.L.Sayon@gmail.com>
2019-09-23 13:31:27 +1000

    Obtained data from server and put onto notificaton

--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -1,31 +1,15 @@
-import { DESC_API, PORT_STATS_API, FLOW_STATS_API, FLOW_AGG_STATS_API } from '../actions/types';
+import {  } from '../actions/types';
 
 const initialState = {
     
 };
 
-
 export default function(state = initialState, action) {
     switch(action.type) {
-        case DESC_API:
+        case NOTIFICATION_ALERT:
             return {
                 ...state,
-                descData : action.payload
-            }
-        case FLOW_AGG_STATS_API:
-            return {
-                ...state,
-                flowAggData: action.payload
-            }
-        case PORT_STATS_API:
-            return {
-                ...state,
-                portData: action.payload
-            }
-        case FLOW_STATS_API:
-            return {
-                ...state,
-                flowData: action.payload
+                machineLearningTriggered : action.payload
             }
         default:
             return state;
