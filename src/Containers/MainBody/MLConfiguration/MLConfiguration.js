82f5c132d434f278d01d5cdd53c26b5bbfe8425a
chrissayon <Christopher.L.Sayon@gmail.com>
2019-09-24 22:58:46 +1000

    Can set and obtain machine threshold from backend

--- src/Containers/MainBody/MLConfiguration/MLConfiguration.js
+++ src/Containers/MainBody/MLConfiguration/MLConfiguration.js
@@ -1,21 +1,79 @@
-import React from 'react'
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import axios from 'axios'
 
-const MLConfiguration = () => {
-    return (
-        <React.Fragment>
-            <TextField
-                id="standard-name"
-                label="Machine Learning Threshold"
-                value={"0-1"}
-                margin="normal"
-            />
-            <Button variant="outlined">
-                Set Threshold Result
-            </Button>
-        </React.Fragment>
-    )
+
+class MLConfiguration extends Component {
+    state = {
+        inputMlThreshold : "",
+        mlThreshold : "",
+    }
+
+    setIP = (event) => {
+        this.setState({
+            inputMlThreshold : event.target.value
+        });
+    }
+
+    setMlThreshold = () => {
+        let self = this;
+        axios.post('http://127.0.0.1:8000/sdn_communication/update_ml_threshold/',{
+            data: { ml_threshold : this.state.inputMlThreshold },
+        })
+        .then((response) => {
+            console.log(response)
+            self.setState({mlThreshold: response.data.ml_threshold})
+        })
+    }
+
+    getMlThreshold = () => {
+        axios.get('http://127.0.0.1:8000/sdn_communication/update_ml_threshold/')
+        .then((response) => {
+            console.log(response)
+            this.setState({mlThreshold: response.data.ml_threshold})
+        })
+    }
+
+    componentDidMount() {
+        this.getMlThreshold();
+    }
+    
+
+    render () {
+        return (
+            <React.Fragment>
+                <Grid container spacing={10} >
+                    <Grid item  >
+                        <TextField
+                            id="standard-name"
+                            label="Set ML Threshold"
+                            value={this.state.inputMlThreshold}
+                            onChange = {this.setIP}
+                            margin="normal"
+                        />
+                    </Grid>
+                    <Grid item  >
+                        <TextField
+                            id="standard-name"
+                            label="ML Threshold Value"
+                            value={this.state.mlThreshold}
+                            onChange = {this.setIP}
+                            margin="normal"
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+                    </Grid>
+                    
+                </Grid>
+                <Button variant="outlined" onClick={this.setMlThreshold}>
+                    Set Machine Learning Threshold
+                </Button>
+            </React.Fragment>
+        )
+    }
 }
 
-export default MLConfiguration
+export default MLConfiguration;
