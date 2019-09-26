7b51a6c96d69666ffc4ea40cf59c1bc2d4992f3a
chrissayon <Christopher.L.Sayon@gmail.com>
2019-09-26 21:16:36 +1000

    Completed one table entry for flow aggregate

--- src/Containers/MainBody/ViewLogs/ViewLogs.js
+++ src/Containers/MainBody/ViewLogs/ViewLogs.js
@@ -18,6 +18,12 @@ import {
   KeyboardDatePicker,
 } from '@material-ui/pickers';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
@@ -32,56 +38,86 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function SimpleSelect() {
-  const classes = useStyles();
-  const [values, setValues] = React.useState({
-    statType: '',
-  });
+    const classes = useStyles();
+    
+    //State for dropdown selection of stats
+    const [values, setValues] = React.useState({
+        statType: '',
+    });
 
-  const [startDate, setStartDate] = React.useState(new Date('2014-08-18T21:11:54'));
-  const [endDate, setEndDate] = React.useState(new Date('2014-08-18T21:11:54'));
-  const [maxRecords, setMaxRecords] = React.useState(1)
+    //State for start and enddate information
+    const [startDate, setStartDate] = React.useState(new Date('2014-08-18T21:11:54'));
+    const [endDate, setEndDate] = React.useState(new Date('2020-08-18T21:11:54'));
+    
+    //State dictating how many records to return from the backend
+    const [maxRecords, setMaxRecords] = React.useState(1)
+    
+    //State for whether View Logs Button has been clicked
+    const [viewLogs, setViewLogs] = React.useState(false)
 
-  // Handler for start date component
-  const handleStartDateChange = date => {
-    setStartDate(date);
-  };
+    //State for log data obtained from backend
+    const [logData, setLogData] = React.useState({})
 
-  // Handler for end date component
-  const handleEndDateChange = date => {
-    setEndDate(date);
-  };
+    //State for header to use in table
+    const [header, setHeader] = React.useState(null)
 
-  const handleRecordChange = event => {
-      setMaxRecords(parseInt(event.target.value))
-  }
- 
+    //State for body of logs
+    const [body, setBody] = React.useState(null)
 
-  const handleChange = event => {
-    setValues(oldValues => ({
-      ...oldValues,
-      [event.target.name]: event.target.value,
-    }));
-  };
+    // Handler for start date component
+    const handleStartDateChange = date => {
+        setStartDate(date);
+    };
 
-  const formatDate = (date) => {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+    // Handler for end date component
+    const handleEndDateChange = date => {
+        setEndDate(date);
+    };
 
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
+    // Handler for record text field compinent
+    const handleRecordChange = event => {
+        setMaxRecords(parseInt(event.target.value))
+    }
 
-    return [year, month, day].join('-');
-}
+    
+    
+    // Handler for stat selection
+    const handleChange = event => {
+        setValues(oldValues => ({
+        ...oldValues,
+        [event.target.name]: event.target.value,
+        }));
+    };
 
-  const logClicked = () => {
-    console.log(values)
-    console.log(formatDate(startDate))
-    console.log(formatDate(endDate))
-    console.log(maxRecords)
+    // Handler for log data from backend server
+    const handleLogChange = responseData => {
+        setLogData(responseData)
+    }
+
+    const formatDate = (date) => {
+        var d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2) 
+            month = '0' + month;
+        if (day.length < 2) 
+            day = '0' + day;
+
+        return [year, month, day].join('-');
+    }
+
+    // React.useEffect(() => {
+    //     console.log(logData)
+    // }, [logData])
+
+    const logClicked = () => {
+        // console.log(values)
+        // console.log(formatDate(startDate))
+        // console.log(formatDate(endDate))
+        // console.log(maxRecords)
+        setViewLogs(true)
 
     if(values.statType === 'flow_aggregate'){
         axios.post('http://127.0.0.1:8000/sdn_communication/flow_agg_stats/',{
@@ -96,91 +132,135 @@ export default function SimpleSelect() {
             },
         })
         .then((response) => {
-            console.log(response)
+            // console.log(response)
+            handleLogChange(response.data)
+            setHeader(
+                <TableRow>
+                    <TableCell>Cell Number</TableCell>
+                    <TableCell>Packet Count</TableCell>
+                    <TableCell>Byte Count</TableCell>
+                    <TableCell>Date Created</TableCell>
+                </TableRow>
+            )
+            setBody(
+                response.data.map((arrayValue, index) => (
+                    <TableRow key={index}>
+                        <TableCell>{index}</TableCell>
+                        <TableCell>{arrayValue.packet_count}</TableCell>
+                        <TableCell>{arrayValue.byte_count}</TableCell>
+                        <TableCell>{arrayValue.created}</TableCell>
+                    </TableRow>
+                ))
+            )
+            // setBody(
+            //     viewLogs.map(row => (
+            //         <TableRow>
+            //             <TableCell>row.</TableCell>
+            //         </TableRow>
+            //     ))
+            // )
+            // console.log(logData)
+            
+
         })
     } 
   }
 
-  return (
-    <Grid container spacing={3}>
-        <Grid item xs={12}>
-            <form autoComplete="off">
-            <FormControl >
-                <InputLabel shrink>
-                    Statistic Type
-                </InputLabel>
-                <Select
-                    value={values.statType}
-                    onChange={handleChange}
-                    inputProps={{
-                        name: 'statType',
-                        id: 'statTypeID',
+    return (
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <form autoComplete="off">
+                <FormControl >
+                    <InputLabel shrink>
+                        Statistic Type
+                    </InputLabel>
+                    <Select
+                        value={values.statType}
+                        onChange={handleChange}
+                        inputProps={{
+                            name: 'statType',
+                            id: 'statTypeID',
+                        }}
+                        displayEmpty
+                        name=""
+                        className={classes.selectEmpty}
+                    >
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={"flow_aggregate"}>Flow Aggregate Statistics</MenuItem>
+                        <MenuItem value={"port_stats"}>Port Statistics</MenuItem>
+                        <MenuItem value={"flow stats"}>Flow Statistics</MenuItem>
+                        <MenuItem value={"attack_notifications"}>Attack Notifications</MenuItem>
+                        <MenuItem value={"flow_aggregate_diff"}>Flow Aggregate Difference Statistics</MenuItem>
+                        <MenuItem value={"port_difference"}>Port Difference Statistics</MenuItem>
+                    </Select>
+                    <FormHelperText>Select desired data</FormHelperText>
+                </FormControl>
+                </form>
+            </Grid>
+            
+            <Grid item xs={12}>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <KeyboardDatePicker 
+                    className={classes.formControl}
+                    disableToolbar
+                    variant="inline"
+                    format="MM/dd/yyyy"
+                    margin="normal"
+                    id="date-picker-inline"
+                    label="Start Date"
+                    value={startDate}
+                    onChange={handleStartDateChange}
+                    KeyboardButtonProps={{
+                        'aria-label': 'change date',
                     }}
-                    displayEmpty
-                    name=""
-                    className={classes.selectEmpty}
-                >
-                    <MenuItem value="">
-                        <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={"flow_aggregate"}>Flow Aggregate Statistics</MenuItem>
-                    <MenuItem value={"port_stats"}>Port Statistics</MenuItem>
-                    <MenuItem value={"flow stats"}>Flow Statistics</MenuItem>
-                    <MenuItem value={"attack_notifications"}>Attack Notifications</MenuItem>
-                    <MenuItem value={"flow_aggregate_diff"}>Flow Aggregate Difference Statistics</MenuItem>
-                    <MenuItem value={"port_difference"}>Port Difference Statistics</MenuItem>
-                </Select>
-                <FormHelperText>Select desired data</FormHelperText>
-            </FormControl>
-            </form>
-        </Grid>
-        
-        <Grid item xs={12}>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <KeyboardDatePicker 
-                className={classes.formControl}
-                disableToolbar
-                variant="inline"
-                format="MM/dd/yyyy"
-                margin="normal"
-                id="date-picker-inline"
-                label="Start Date"
-                value={startDate}
-                onChange={handleStartDateChange}
-                KeyboardButtonProps={{
-                    'aria-label': 'change date',
-                }}
+                    />
+                    <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    format="MM/dd/yyyy"
+                    margin="normal"
+                    id="date-picker-inline"
+                    label="End Date"
+                    value={endDate}
+                    onChange={handleEndDateChange}
+                    KeyboardButtonProps={{
+                        'aria-label': 'change date',
+                    }}
+                    />
+                </MuiPickersUtilsProvider>
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                    id="standard-number"
+                    label="Max Records"
+                    helperText="Please input an integer"
+                    margin="normal"
+                    type="number"
+                    onChange={handleRecordChange}
                 />
-                <KeyboardDatePicker
-                disableToolbar
-                variant="inline"
-                format="MM/dd/yyyy"
-                margin="normal"
-                id="date-picker-inline"
-                label="End Date"
-                value={endDate}
-                onChange={handleEndDateChange}
-                KeyboardButtonProps={{
-                    'aria-label': 'change date',
-                }}
-                />
-            </MuiPickersUtilsProvider>
+            </Grid>
+            <Grid item xs={12}>
+                <Button variant="outlined" onClick={logClicked}>
+                    View Logs
+                </Button>
+            </Grid>
+            <Grid item xs={12}>
+                <Table>
+                    <TableHead>
+                        {
+                            header
+                        }
+                    </TableHead>
+                    <TableBody>
+                        {
+                            body
+                        }
+                        
+                    </TableBody>
+                </Table>
+            </Grid>
         </Grid>
-        <Grid item xs={12}>
-            <TextField
-                id="standard-number"
-                label="Max Records"
-                helperText="Please input an integer"
-                margin="normal"
-                type="number"
-                onChange={handleRecordChange}
-            />
-        </Grid>
-        <Grid item xs={12}>
-            <Button variant="outlined" onClick={logClicked}>
-                View Logs
-            </Button>
-        </Grid>
-    </Grid>
-  );
+    );
 }
