0c66420251f88b34d19a8b8a4e2874d6b46f4152
chrissayon <Christopher.L.Sayon@gmail.com>
2019-09-27 16:05:20 +1000

    Flow Aggregate can now do post requests

--- src/Components/Graph/FlowAggregateGraph.js
+++ src/Components/Graph/FlowAggregateGraph.js
@@ -1,95 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
-import {
-    LineChart, Line, ResponsiveContainer, Scatter, ScatterChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-} from 'recharts';
-  
-const data = [
-    {
-      name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-    },
-    {
-      name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-    },
-    {
-      name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-    },
-    {
-      name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-    },
-    {
-      name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-    },
-    {
-      name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-    },
-    {
-      name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-    },
-];
+import moment from 'moment';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+const data = [{name: 'Page A', uv: 400}, {name: 'Page B', uv: 800}];
 
-let xValue = 0;
 
-class FlowAggregateGraph extends Component {
-    state = {
-        portList : [
-            // // { "date": "2019-08-15T21:24:55.980415+10:00" , "y": 7000 },
-            // // { "date": "2019-08-16T21:24:55.980415+10:00" , "y": 8000 }
-            // { x: 5, y: 9000 },
-            // { x: 10, y: 10000}
-        ]
+const FlowAggregateGraph = (props) => {
+    const [graphData , setGraphData] = React.useState(null)
+
+    const flowAggPost = () => {
+        axios.post('http://127.0.0.1:8000/sdn_communication/flow_agg_diff_graph/',{
+            data: { 
+                maxRecords: 7
+            },
+        })
+        .then((response) => {
+            console.log(response.data)
+            setGraphData(
+                response.data.map((arrayValue, index) => ({
+                    number: index,
+                    packet_count: arrayValue.packet_count,
+                    byte_count: arrayValue.byte_count,
+                    created : moment(arrayValue.created).format('DD/MM/YYYY h:mm:ss')
+                }))
+            )
+        })
     }
 
-    componentDidMount() {
-        this.getData(); //Get data first time
-        this.interval = setInterval(() => { //wait 5 seconds and rerun it
-            this.getData();
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+           
+          console.log("In Set")
         }, 5000);
-    }
-
-
-    componentWillUnmount() {
-        clearInterval(this.interval)
-    }
-
-    shouldComponentUpdate(nextProps, nextState){
-        return this.state != nextState
-    }
-
-
-    getData() {
-        axios.get('http://127.0.0.1:8000/sdn_communication/flow_agg_stats/')
-            .then((response) => {
-                let graphData = this.state;
-                xValue += 5;
-                graphData.portList.push({
-                    //time: response.data.last_modified, 
-                    time: xValue, 
-                    byte_count: response.data.byte_count 
-                })
-                // console.log(graphData)
-                this.setState(
-                    graphData
-                )
-            })
-    }
+        return () => {
+          clearInterval(interval);
+        };
+    }, []);
     
-    
-    render () {
-        console.log(this.state)
-        return (
-        <ResponsiveContainer key={xValue} width="99%" height="99%">
-            <LineChart width={630} height={250} data={this.state.portList}
-                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                <Tooltip />
-                <Line type="monotone" dataKey="byte_count" stroke="#8884d8" />
-                <CartesianGrid stroke="#ccc" />
-                <XAxis dataKey="time" />
-                <YAxis dataKey="byte_count"/>
-            </LineChart>
-        </ResponsiveContainer>
-        )
-    }
+
+    return (
+        <LineChart width={600} height={300} data={graphData}>
+            <Line type="monotone" dataKey="byte_count" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="packet_count" />
+            <YAxis />
+        </LineChart>
+    )
 };
 
 export default FlowAggregateGraph;
