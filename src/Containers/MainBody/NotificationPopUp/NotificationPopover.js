8b55ce2ce1879e78a2713dafda99e8cb44d8a83c
chrissayon <Christopher.L.Sayon@gmail.com>
2019-09-29 19:31:18 +1000

    Removed redux from notifications

--- src/Containers/MainBody/NotificationPopUp/NotificationPopover.js
+++ src/Containers/MainBody/NotificationPopUp/NotificationPopover.js
@@ -5,8 +5,6 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 
 import NotificationIcon from '@material-ui/icons/Notifications'
-import { connect } from 'react-redux';
-import { notificationRequests } from '../../../actions/alertRequests'
 
 const useStyles = makeStyles(theme => ({
   typography: {
@@ -14,70 +12,50 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-function NotificationPopover(props) {
-const classes = useStyles();
-const [anchorEl, setAnchorEl] = React.useState(null);
+function NotificationPopover() {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
-function handleClick(event) {
-    setAnchorEl(event.currentTarget);
-    // dispatch({ type: notificationRequests })
-    props.notificationRequests();
-};
+    function handleClick(event) {
+        setAnchorEl(event.currentTarget);
+    };
 
-function handleClose() {
-    setAnchorEl(null);
-};
+    function handleClose() {
+        setAnchorEl(null);
+    };
 
-const open = Boolean(anchorEl);
-const id = open ? 'simple-popover' : undefined;
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
 
-console.log(props.notifications);
-let checking = (props.notifications.machineLearningTriggered === undefined)
-console.log(checking)
+    return (
+        <React.Fragment>
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleClick}
+            >
+                <NotificationIcon />
+            </IconButton>
 
-return (
-    <React.Fragment>
-        <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleClick}
-        >
-            <NotificationIcon />
-        </IconButton>
-
-        <Popover
-            id={id}
-            open={open}
-            anchorEl={anchorEl}
-            onClose={handleClose}
-            anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-            }}
-            transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-            }}
-        >
-            { 
-                props.notifications.machineLearningTriggered === undefined ? (
-                    null
-                ) : (
-                    <Typography className={classes.typography}>{props.notifications.machineLearningTriggered[0].attack_type}</Typography>
-                    //<Typography className={classes.typography}>Notifications</Typography>
-                )
-            }
-        </Popover>
-    </React.Fragment>
-);
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+                }}
+                transformOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+                }}
+            >
+                Test
+            </Popover>
+        </React.Fragment>
+    );
 }
 
-const mapStateToProps = function(state) {
-    return {
-        notifications: state.notificationData
-    }
-}
-  
-  //export default NotificationPopover;
-export default connect(mapStateToProps,{notificationRequests})(NotificationPopover);
+export default NotificationPopover;
